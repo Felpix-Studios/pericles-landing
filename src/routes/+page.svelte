dee96c9ca4b2a6242d7bc40e93e409cde9a3cc8b
Kevin Li <jshiansongli@gmail.com>
2023-10-31 15:04:26 -0700

    oooh ahhh gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,6 +25,22 @@
 
     h1 {
         color: var(--color-title);
-        font-size: 60px;
+        font-size: 96px;
+        background: linear-gradient(270deg, var(--color-title), var(--color-title-2), var(--color-title));
+        background-size: 200% 200%;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        animation: gradient 5s linear infinite;
+    }
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
     }
 </style>
